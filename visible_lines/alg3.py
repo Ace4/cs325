53bb9b6f1305ad14ce19cf75fe11edc10f1bcc65
ace4 <acegger4@gmail.com>
2014-10-15 17:08:55 -0700

    initial commit of alg3.py

---
+++ visible_lines/alg3.py
@@ -0,0 +1,62 @@
+from random import randrange
+from time import clock 
+
+num_lines = 900
+if num_lines < 3:
+	exit() 
+
+slopes = []
+intercepts = []
+visible = [] 
+
+#generate unique random lines
+def genLines(n):
+        for i in range(0, n):
+                rand_slope = randrange(-n,n);
+                while slopes.count(rand_slope) != 0:
+                        rand_slope = randrange(-n,n)
+
+                slopes.append(rand_slope)
+                intercepts.append(randrange(-n, n))
+
+        slopes.sort()
+
+
+#for the one true wizard 
+def alg1(n):
+	for i in range(0,n):
+		visible.append(True)
+
+	for n in range(2, n): 
+		j = n-2
+		i = n-1
+		k = n
+	
+		YjYk = slopes[j]*(intercepts[j] - intercepts[k]) + intercepts[j]*(slopes[k] - slopes[j])
+		YiYk = slopes[i]*(intercepts[j] - intercepts[k]) + intercepts[i]*(slopes[k] - slopes[j])
+	
+		if(YjYk > YiYk):
+			visible[i] = False 
+
+def alg2(n):
+        for i in range(0, n):
+                visible.append(True)                            #mark each line as visible
+
+        for k in range(2, n):                                   #check visibility of all triplets where i<j<k
+                for i in range(1, n-1):
+                        if visible[i]:
+                                for j in range(0, n-2):
+                                        if(j < i < k):
+                                                YjYk = slopes[j]*(intercepts[j] - intercepts[k]) + intercepts[j]*(slopes[k] - slopes[j])
+                                                YiYk = slopes[i]*(intercepts[j] - intercepts[k]) + intercepts[i]*(slopes[k] - slopes[j])
+                                                if(YjYk > YiYk):
+                                                        visible[i] = False
+
+
+genLines(num_lines)
+t0 = clock()
+alg1(num_lines)
+alg2(num_lines)
+t1 = clock()
+
+print t1 - t0
