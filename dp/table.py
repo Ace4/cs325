dbd95ceb5200e36d57dd685feb534fe5da092d90
ace4 <acegger4@gmail.com>
2014-11-13 19:33:52 -0800

    added init to DPentry

--- dp/table.py
+++ dp/table.py
@@ -1,10 +1,11 @@
 class DPentry:
-	value = None
-	prev = None
+	def __init__(self):
+		value = None
+		prev = None
 
 class DPtable:
 	def __init__(self, n):
-		self.table = [[DPentry for x in xrange(n)] for x in xrange(n)]
+		self.table = [[DPentry() for y in range(n)] for x in range(n)]
 		self.max_value = "-Inf"
 		self.max_index = []
 		self.path = []
