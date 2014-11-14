a9dd426847f8bc7637045dd8e43d76d3ce56a9f8
John Cargill <cargillj@onid.oregonstate.edu>
2014-11-13 21:23:16 -0800

    added a print_table function

--- dp/table.py
+++ dp/table.py
@@ -9,3 +9,14 @@ class DPtable:
 		self.max_value = "-Inf"
 		self.max_index = []
 		self.path = []
+
+def print_table(input_table):
+	n = len(input_table[0])
+	table = []
+	row = []
+	for x in range(0, n):
+		for y in range(0, n):
+			row.append(input_table[x][y].value)
+		table.append(row)
+		row = []
+	print(table)
