8936da69610af6e961b8507b62a44d4407c14102
cargillj <cargillj@onid.oregonstate.edu>
2014-11-19 21:02:24 -0800

    working numpy table maker

--- dp3/dp.py
+++ dp3/dp.py
@@ -1,18 +1,17 @@
 import numpy as np
 from time import clock
 
-rows = 10000
-cols = 10000
+def make_table_from_file(fn):
+	f = open(fn)
+	rows = int(f.readline())
+	cols = int(f.readline())
 
-def make_blank_table(rows, cols):
-	result = []
-	for x in range(0, rows):
-		result.append([])
-		for y in range(0, cols):
-			result[x].append(0)
+	result = np.array([f.readline().split()], dtype=np.int32)
+	for x in range(1, rows):
+		row = f.readline().split()
+		nprow = np.array([row], dtype=np.int32)
+		result = np.concatenate((result, nprow), axis=0)
 	return result
 
-t0 = clock()
-x = np.zeros((rows, cols))
-t1 = clock()
-print('numpy: ' + repr(t1-t0))
+A = make_table_from_file('example-input-2.txt')
+print(A)
