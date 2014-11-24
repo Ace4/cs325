53fc325f006f4001595a796c31b08cdbd2c696ab
ace4 <acegger4@gmail.com>
2014-11-24 11:17:03 -0800

    made a vec2 for dealing with 2d computation

---
+++ tsp/vec2.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#ifndef VEC3_H
+#define VEC3_H
+
+class Vec2
+{
+    protected:
+        float v0, v1;
+
+    public:
+        Vec2( float=0, float=0);
+        Vec2&   operator=( const Vec2& );
+        Vec2    operator+( const Vec2& );
+        Vec2    operator-( const Vec2& );       // binary -
+        Vec2    operator-( );                   // unary -
+        float    Cross( Vec2& );
+        float   Dot( Vec2& );
+        float   Length( );
+        void    Print( char * = "", FILE * = stderr );
+        Vec2    Unit( );
+
+	friend class Mat4;
+};
+
+inline float 
+SWR( float f)
+{
+	return f * f; 
+}
+
+#endif
+
