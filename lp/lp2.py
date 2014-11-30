05ab96b1f3c902c823d78a5e14ab7292d6126ef5
John Cargill <cargillj@onid.oregonstate.edu>
2014-11-29 18:37:04 -0800

    problem 2 is still a hit and a miss

--- lp/lp2.py
+++ lp/lp2.py
@@ -11,18 +11,43 @@ from pulp import *
 a = LpVariable('a',None,None,LpInteger)
 b = LpVariable('b',None,None,LpInteger)
 c = LpVariable('c',None,None,LpInteger)
+z = ['p1','p2','p3','p4','p5','p6','p7']
 
 x = [1,2,3,5,7,8,10]
 y = [3,5,7,11,14,15,19]
 
+# x = {
+# 	 'p1': 1,
+# 	 'p2': 2,
+# 	 'p3': 3,
+# 	 'p4': 5,
+# 	 'p5': 7,
+# 	 'p6': 8,
+# 	 'p7': 10}
+
+# y = {
+# 	 'p1': 3,
+# 	 'p2': 5,
+# 	 'p3': 7,
+# 	 'p4': 11,
+# 	 'p5': 14,
+# 	 'p6': 15,
+# 	 'p7': 19}
+
 # Create the 'prob' variable to contain the problem data
 prob = LpProblem("notleastsquares",LpMinimize)
 
-# The objective function is added to 'prob' first
-prob += ???
+# A dictionary called 'Points' is created to contain the referenced Variables
+Points = LpVariable.dicts("Points", z)
 
-# Other constraints are added to the problem
-prob += a != b != c != 0, "NotZerosRequirement"
+# First constraint is the objective
+prob += lpSum([a*x[i] + b*y[i] - c for i in range(len(z))]), "MinimizeObjective"
+
+# Add maximum absolute value constraints
+for i in range(len(z)):
+	j = z[i]
+	prob += Points[j] >= a*x[i] + b*y[i] - c, "PositiveAbsoluteRequiement_%s" % Points[j]
+	prob += Points[j] >= -a*x[i] - b*y[i] + c, "NegativeAbsoluteRequiement_%s" % Points[j]
 
 # The problem data is written to an .lp file
 prob.writeLP("notleastsquares.lp")
@@ -38,4 +63,4 @@ for v in prob.variables():
     print(v.name, "=", v.varValue)
 
 # The optimised objective function value is printed to the screen
-print("Optimized fit = ", value(prob.objective))
+print("Maximum Absolute Deviation = ", value(prob.objective))
